081d6214d2f60f0fb347976eba784446ff5d687e
ahmed-tahseen <54220580+ahmed-tahseen@users.noreply.github.com>
2019-09-24 21:19:43 +0300

    AT-Root

---
+++ AT-Root.py
@@ -0,0 +1,7 @@
+v = "\033[0;31m"
+print (v+"By Ahmed Tahseen")
+print (v+"YouTube Iraq4 Linux")
+import os
+os.system ("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Parrot/parrot.sh && bash parrot.sh")
+print ("By Ahmed Tahseen")
+print ("YouTube Iraq4 Linux")
